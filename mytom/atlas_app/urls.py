c371a461595fb8c7985838ae9694b35f3a18a747
Ezequiel Contreras Martinez <ezequiel@lco.global>
2023-08-02 10:31:53 -0700

    second working version of photometry django app now with loading feature

--- mytom/atlas_app/urls.py
+++ mytom/atlas_app/urls.py
@@ -8,5 +8,5 @@ app_name = 'atlas_app'
 
 urlpatterns = [
 	path("<int:pk>/query/",views.QueryView.as_view(),name='query'),
-	path('<int:pk>/', views.TargetDetailView.as_view(),name='detail')
+	path('<int:pk>/', views.TargetDetailView.as_view(),name='detail'),
 ]
